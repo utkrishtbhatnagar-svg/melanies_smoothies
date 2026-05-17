2b7d6d0a0f4b064a73b160edf40aef396768c049
utkrishtbhatnagar-svg <utkrishtbhatnagar@gmail.com>
2026-05-17 14:35:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,5 +36,12 @@ if ingredients_list:
     time_to_insert = st.button('Submit Order')
 
     if time_to_insert:
+
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
+
+
+#New section to display smoothiefroot nutrition information
+import requests  
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+st.text(smoothiefroot_response)
