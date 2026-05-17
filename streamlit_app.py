6ab2d624949be26fb9e51f09be5d581ad5ed78ec
utkrishtbhatnagar-svg <utkrishtbhatnagar@gmail.com>
2026-05-17 15:00:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests 
 
 st.title("Customize your Smoothie :cup_with_straw:")
 st.write(
@@ -26,7 +27,9 @@ if ingredients_list:
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '
+      ingredients_string += fruit_chosen + ' '
+      smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+      sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     st.write(ingredients_string)
 
@@ -44,8 +47,6 @@ if ingredients_list:
 
 
 
-#New section to display smoothiefroot nutrition information
+
 import requests  
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-# st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
