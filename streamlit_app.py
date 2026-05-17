f1ff74acaee16954b7193d3d3614e5326e9f2cae
utkrishtbhatnagar-svg <utkrishtbhatnagar@gmail.com>
2026-05-17 14:39:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,6 +41,9 @@ if ingredients_list:
         st.success('Your Smoothie is ordered!', icon="✅")
 
 
+
+
+
 #New section to display smoothiefroot nutrition information
 import requests  
 smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
